672465c01569263418ba3316894079b78b3e443b
AustenHsiao <Austenhsiao@gmail.com>
2021-04-18 02:58:45 -0700

    Finished feature matching w/ sift

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 ''' 
 Computer vision and deep learning. Written by Austen Hsiao
 '''
+from sift import *
+from clustering import *  # for part 2
 from filters import *  # for part 1
 '''
 class Filter(filename); a filename is supplied to the class. The filename will be for an image file
@@ -11,17 +13,20 @@ public class methods:
     applyDoG(direction); A direction is supplied as a char, 'x' or 'y'. Applies the DoG filter. Returns an np array 
     applySobel(); Applies the Sobel filter using DoG in both directions. Returns an np array
 '''
-from clustering import *  # for part 2
 '''
 class Kcluster(filename, clusternum); a filename and number of clusters is supplied to the class. The file can be data (mx2) or an image
 public class methods: 
     graph(r); Runs the algorithm a total of r times, then creates a graph out of the run with the lowest sum of squares error
 '''
-from sift import *
+'''
+class Sift(); No parameters needed
+public class methods:
+    featureMatch(file1, file2); Creates a jpg image that draws a line between the top 10% of matched features (From file1 to file2).
+'''
 
 if __name__ == '__main__':
-    #Filter("images/filter1_img.jpg").showcase()
+    # Filter("images/filter1_img.jpg").showcase()
     #Kcluster("data/510_cluster_dataset.txt", 8).graph(10)
     #Kcluster("images/small.jpg", 5).graph(1)
-    #Sift().topTenPMatches("images/SIFT1_img.jpg", "images/SIFT2_img.jpg")
-    Sift().getKeyPoints("images/SIFT2_img.jpg")
+    #Sift().featureMatch("images/test1.jpg", "images/test2.jpg")
+    # Sift().getKeyPoints("images/SIFT2_img.jpg")
