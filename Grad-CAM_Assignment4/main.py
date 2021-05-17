171ccf527799387cf98d544511d5c9923b89c1ab
AustenHsiao <Austenhsiao@gmail.com>
2021-05-17 13:01:29 -0700

    Need to add final layers to decoder

--- Grad-CAM_Assignment4/main.py
+++ Grad-CAM_Assignment4/main.py
@@ -1,28 +1,37 @@
 import tensorflow as tf
 import numpy as np
-import cv2
+#import cv2
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, Convolution2D, MaxPooling2D, UpSampling2D, Reshape
 #from keras.utils import np_utils
 
 if __name__ == '__main__':
+    #########################
     # Load Data
+    #########################
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
 
+    #########################
     # Normalize
+    #########################
     x_train = np.divide(x_train, 255.0)
     x_test = np.divide(x_test, 255.0)
+    y_train = np.divide(y_train, 255.0)
+    y_test = np.divide(y_test, 255.0)
+
+    x_train = x_train.reshape(60000,28,28,1)
+    x_test  = x_test.reshape(10000,28,28,1)
+
     
-    #cv2.imshow("train0", x_train[59999])
-    #cv2.waitKey(0)
-    
+    #########################
     # CAE Architecture
+    #########################
     encoder = Sequential(name='encoder')
-    encoder.add(Convolution2D(32, (3, 3), strides=(1,1), activation='relu', input_shape=(1, 28, 28), data_format='channels_first', padding='same'))
-    encoder.add(MaxPooling2D(data_format='channels_first'))
-    encoder.add(Convolution2D(32, (3, 3), strides=(1,1), activation='relu', input_shape=(1, 14, 14), data_format='channels_first', padding='same'))
-    encoder.add(MaxPooling2D(data_format='channels_first'))
-    encoder.add(Convolution2D(32, (3, 3), strides=(1,1), activation='relu', input_shape=(1, 7, 7), data_format='channels_first', padding='same'))
+    encoder.add(Convolution2D(filters=32, kernel_size=3, activation='relu', input_shape=(28, 28, 1), padding='same'))
+    encoder.add(MaxPooling2D(pool_size=2))
+    encoder.add(Convolution2D(filters=32, kernel_size=3, activation='relu', padding='same'))
+    encoder.add(MaxPooling2D(pool_size=2))
+    encoder.add(Convolution2D(filters=32, kernel_size=3, activation='relu', padding='same'))
     #encoder.summary()
 
     middle = Sequential(name='bottleNeck')
@@ -33,11 +42,12 @@ if __name__ == '__main__':
     decoder = Sequential(name='decoder')
     decoder.add(Dense(1568, activation='relu'))
     decoder.add(Reshape((32, 7, 7)))
-    decoder.add(Convolution2D(32, (3, 3), strides=(1,1), activation='relu', input_shape=(1, 7, 7), data_format='channels_first', padding='same'))
-    decoder.add(UpSampling2D(data_format='channels_first'))
-    decoder.add(Convolution2D(32, (3, 3), strides=(1,1), activation='relu', input_shape=(1, 14, 14), data_format='channels_first', padding='same'))
-    decoder.add(UpSampling2D(data_format='channels_first'))
-    decoder.add(Convolution2D(32, (3, 3), strides=(1,1), activation='relu', input_shape=(1, 28, 28), data_format='channels_first', padding='same'))
+    decoder.add(Convolution2D(filters=32, kernel_size=3, activation='relu', input_shape=(7, 7, 1), padding='same'))
+    decoder.add(UpSampling2D())
+    decoder.add(Convolution2D(filters=32, kernel_size=3, activation='relu', input_shape=(14, 14, 1), padding='same'))
+    decoder.add(UpSampling2D())
+    decoder.add(Convolution2D(filters=32, kernel_size=3, activation='relu', input_shape=(28, 28, 1), padding='same'))
+    decoder.add() ##<----- ADD HERE
     #decoder.summary()
 
     CAE = Sequential()
@@ -45,3 +55,10 @@ if __name__ == '__main__':
     CAE.add(middle)
     CAE.add(decoder)
     CAE.summary()
+
+    #########################
+    # Train
+    #########################
+    CAE.compile(optimizer='adam', loss=tf.keras.losses.binary_crossentropy)
+    CAE.fit(x_train, y_train, batch_size=64, verbose=2, epochs=5)
+    #print(x_train.shape)
